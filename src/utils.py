43817f439236943f34e2d8ab574336eec0493244
lukasz-kedzierski <lukasz.kedzierski96@gmail.com>
2024-05-03 10:30:52 +0200

    Added descriptions.

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,17 @@ from scipy import signal
 
 
 def select_sequence(dataframe, selected_columns, window_length, stride, sampling_frequency=None, differencing=False):
+    """
+    Draw random sample from run.
+    Args:
+        dataframe: data from run
+        selected_columns: subset of columns representing chosen signal channels
+        window_length: number of time steps in window
+        stride: how many time steps to omit from original data
+        sampling_frequency: if not None, parse this argument to highpass filter and filter data
+        differencing: if True, calculate differences between consecutive time steps
+    return: sequence dataframe containing selected channels and time steps
+    """
     dataframe = dataframe[selected_columns]
     initial_timestep = random.randint(0, (len(dataframe) - 1) - window_length)
     final_timestep = initial_timestep + window_length
@@ -17,6 +28,15 @@ def select_sequence(dataframe, selected_columns, window_length, stride, sampling
 
 
 def butter_highpass_filter(data, cutoff, fs, order=5):
+    """
+    Highpass filter.
+    Args:
+        data: signal to filter
+        cutoff: filter cutoff frequency
+        fs: signal sampling frequency
+        order: filter order
+    return: filtered signal
+    """
     nyq = 0.5 * fs
     normal_cutoff = cutoff / nyq
     b, a = signal.butter(order, normal_cutoff, btype='high', analog=False, output='ba')
