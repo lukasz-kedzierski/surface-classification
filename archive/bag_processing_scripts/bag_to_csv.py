33a0db72167a0e5614d338e46efccbb3419cbb2a
lukasz-kedzierski <lukasz.kedzierski96@gmail.com>
2024-06-26 23:31:00 +0200

    Converted data extracting notebooks to scripts.

---
+++ archive/bag_processing_scripts/bag_to_csv.py
@@ -0,0 +1,17 @@
+from bagpy import bagreader
+from pathlib import Path
+
+# set path to bag files
+TRAIN = True
+
+if TRAIN:
+    MAIN_DIR = Path('../data/train_set/fixed/')
+else:
+    MAIN_DIR = Path('../data/test_set/bags/')
+
+# read bags from directory
+bag_paths = list(MAIN_DIR.rglob('*.bag'))
+for bag_path in bag_paths:
+    b = bagreader(bag_path.__str__())
+    for topic in b.topics:
+        b.message_by_topic(topic)
