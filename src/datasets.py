18fd866ed1fb294c2d6024f501ae863d2f6a61e8
lukasz-kedzierski <lukasz.kedzierski96@gmail.com>
2024-09-29 21:05:53 +0200

    Updated variable names, docstrings, and type hinting in datasets.py.

--- src/datasets.py
+++ src/datasets.py
@@ -1,65 +1,97 @@
 import numpy as np
 import pandas as pd
-
 import torch
+
 from torch.utils.data import Dataset
 
 from utils import get_sample_features, sample_sequence, sequence_run
 
 
 class SurfaceDataset(Dataset):
-    def __init__(self, samples, labels, sample_freq=20., data_freq=100., lookback=1., subset=None):
+    def __init__(
+            self,
+            runs: list | np.array,
+            labels: np.array,
+            data_freq: float = 100.,
+            sample_freq: float | None = None,
+            lookback: float = 1.,
+            subset: list | None = None,
+    ):
         """
+        Dataset for training a CNN on several registered runs.
         Args:
-            samples: array of time series, first dimension is number of time steps
-            labels: array of surface labels
-            sample_freq: lowest frequency of collected data
-            data_freq: highest frequency of collected data
-            lookback: size of window for prediction
-            subset: list containing data categories
+            runs: list or array of time series file names
+            labels: ndarray of one-hot encoded surface labels
+            data_freq: frequency of collected data [Hz]
+            sample_freq: new sampling frequency of windowed data [Hz]
+            lookback: size of window for prediction [s]
+            subset: list containing data sources
         """
 
-        self.samples = samples
+        self.runs = runs
         self.labels = labels
-        self.sampling_frequency = sample_freq
         self.dataset_frequency = data_freq
+        if sample_freq is None:
+            self.sampling_frequency = data_freq
+        else:
+            self.sampling_frequency = sample_freq
         self.lookback = lookback * self.sampling_frequency
+
         self.stride = int(self.dataset_frequency / self.sampling_frequency)
         self.window_length = int(self.lookback * self.stride)
-        self.subset = subset
-        if not self.subset:
+
+        if subset is None:
             self.subset = ('imu', 'servo')
-        measurements = {
+        modalities = {
             'cmd_vel': ['linear.x', 'angular.z'],
-            'imu': ['linear_acceleration.x', 'linear_acceleration.y', 'linear_acceleration.z', 'angular_velocity.x',
-                    'angular_velocity.y', 'angular_velocity.z'],
+            'imu': ['linear_acceleration.x', 'linear_acceleration.y', 'linear_acceleration.z',
+                    'angular_velocity.x', 'angular_velocity.y', 'angular_velocity.z'],
             'odom': ['pose.pose.position.x', 'pose.pose.position.y', 'twist.twist.linear.x', 'twist.twist.angular.z'],
             'servo': ['mean_power_left', 'mean_power_right'],
         }
-        self.selected_columns = []
-        for measurement in self.subset:
-            self.selected_columns.extend(measurements[measurement])
+        self.selected_modalities = []
+        for source in self.subset:
+            self.selected_modalities.extend(modalities[source])
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.runs)
 
     def __getitem__(self, idx):
         """
-        Retrieve a single, windowed time series from dataset
+        Retrieves a single, windowed time series from dataset
         """
 
-        sample = self.samples[idx]
-        run = pd.read_csv(sample, index_col=[0]).drop(labels='Time', axis=1)
+        run = self.runs[idx]
+        time_series = pd.read_csv(run, index_col=[0]).drop(labels='Time', axis=1)
 
-        X = sample_sequence(run, self.selected_columns, self.window_length, self.stride)
+        X = sample_sequence(time_series, self.selected_modalities, self.window_length, self.stride)
         y = self.labels[idx]
 
         return torch.tensor(X, dtype=torch.float), torch.tensor(y, dtype=torch.float)
 
 
 class SurfaceDatasetXGB(SurfaceDataset):
-    def __init__(self, samples, labels, sample_freq=20., data_freq=100., lookback=1., subset=None):
-        super().__init__(samples, labels, sample_freq, data_freq, lookback, subset)
+    def __init__(
+            self,
+            runs: list | np.array,
+            labels: np.array,
+            data_freq: float = 100.,
+            sample_freq: float | None = None,
+            lookback: float = 1.,
+            subset: list | None = None,
+    ):
+        """
+        Dataset for training XGBoost on several registered runs.
+        Args:
+            runs: list or array of time series file names
+            labels: ndarray of one-hot encoded surface labels
+            data_freq: frequency of collected data [Hz]
+            sample_freq: new sampling frequency of windowed data [Hz]
+            lookback: size of window for prediction [s]
+            subset: list containing data sources
+        """
+
+        super().__init__(runs, labels, data_freq, sample_freq, lookback, subset)
         engineered_time_features = [channel + '_t_' + feature for feature in (
             'min',
             'max',
@@ -73,25 +105,25 @@ class SurfaceDatasetXGB(SurfaceDataset):
             'crest',
             'form',
             'pulse',
-        ) for channel in self.selected_columns]
+        ) for channel in self.selected_modalities]
         engineered_freq_features = [channel + '_f_' + feature for feature in (
             'sum',
             'max',
             'mean',
             'peak',
             'var',
-        ) for channel in self.selected_columns]
+        ) for channel in self.selected_modalities]
         self.engineered_features = np.array(engineered_time_features + engineered_freq_features)
 
     def __getitem__(self, idx):
         """
-        Retrieve a single, windowed time series from dataset
+        Retrieves a single, windowed time series from dataset
         """
 
-        sample = self.samples[idx]
-        run = pd.read_csv(sample, index_col=[0]).drop(labels='Time', axis=1)
+        run = self.runs[idx]
+        time_series = pd.read_csv(run, index_col=[0]).drop(labels='Time', axis=1)
 
-        X = sample_sequence(run, self.selected_columns, self.window_length, self.stride)
+        X = sample_sequence(time_series, self.selected_modalities, self.window_length, self.stride)
         X_hat = get_sample_features(X)
         y = self.labels[idx]
 
@@ -99,44 +131,56 @@ class SurfaceDatasetXGB(SurfaceDataset):
 
 
 class InferenceDataset(Dataset):
-    def __init__(self, run, sample_freq=20., data_freq=100., lookback=1., subset=None):
+    def __init__(
+            self,
+            run: str,
+            data_freq: float = 100.,
+            sample_freq: float | None = None,
+            lookback: float = 1.,
+            subset: list | None = None,
+    ):
         """
+        Dataset for a CNN inference on a single registered run.
         Args:
-            run: array of time series, first dimension is number of time steps
-            sample_freq: lowest frequency of collected data
-            data_freq: highest frequency of collected data
-            lookback: size of window for prediction
-            subset: list containing data categories
+            run: time series file name
+            data_freq: frequency of collected data [Hz]
+            sample_freq: new sampling frequency of windowed data [Hz]
+            lookback: size of window for prediction [s]
+            subset: list containing data sources
         """
 
         self.run = pd.read_csv(run, index_col=[0]).drop(labels='Time', axis=1)
-        self.sampling_frequency = sample_freq
         self.dataset_frequency = data_freq
+        if sample_freq is None:
+            self.sampling_frequency = data_freq
+        else:
+            self.sampling_frequency = sample_freq
         self.lookback = lookback * self.sampling_frequency
+
         self.stride = int(self.dataset_frequency / self.sampling_frequency)
         self.window_length = int(self.lookback * self.stride)
-        self.subset = subset
-        if not self.subset:
+
+        if subset is None:
             self.subset = ('imu', 'servo')
-        measurements = {
+        modalities = {
             'cmd_vel': ['linear.x', 'angular.z'],
             'imu': ['linear_acceleration.x', 'linear_acceleration.y', 'linear_acceleration.z', 'angular_velocity.x',
                     'angular_velocity.y', 'angular_velocity.z'],
             'odom': ['pose.pose.position.x', 'pose.pose.position.y', 'twist.twist.linear.x', 'twist.twist.angular.z'],
             'servo': ['mean_power_left', 'mean_power_right'],
         }
-        self.selected_columns = []
-        for measurement in self.subset:
-            self.selected_columns.extend(measurements[measurement])
+        self.selected_modalities = []
+        for source in self.subset:
+            self.selected_modalities.extend(modalities[source])
 
-        self.samples = sequence_run(self.run, self.selected_columns, self.window_length, self.stride)
+        self.samples = sequence_run(self.run, self.selected_modalities, self.window_length, self.stride)
 
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, idx):
         """
-        Retrieve a single, windowed time series from dataset
+        Retrieves a single, windowed time series from dataset
         """
 
         X = self.samples[idx]
