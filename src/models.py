4c6e892108156b557ba47ef414827b16b68d2b11
lukasz-kedzierski <lukasz.kedzierski96@gmail.com>
2025-08-18 19:11:37 +0200

    Added docstrings to CNN model.

--- src/models.py
+++ src/models.py
@@ -1,27 +1,62 @@
-import torch.nn as nn
+"""Module for CNN-based surface classification model."""
+
+from torch import nn
 
 
 class BasicBlock(nn.Module):
+    """Basic block for the CNN model, consisting of convolutional and batch norm layers."""
     def __init__(self, inplanes, outplanes, stride):
-        super(BasicBlock, self).__init__()
-        self.conv = nn.Conv1d(in_channels=inplanes, out_channels=outplanes, kernel_size=3, stride=stride, padding=1)
+        """
+        Parameters
+        ----------
+        inplanes : int
+            Number of input channels.
+        outplanes : int
+            Number of output channels.
+        stride : int
+            Stride for the convolutional layer.
+        """
+        super().__init__()
+        self.conv = nn.Conv1d(
+            in_channels=inplanes,
+            out_channels=outplanes,
+            kernel_size=3,
+            stride=stride,
+            padding=1
+            )
         self.bn = nn.BatchNorm1d(num_features=outplanes)
 
     def forward(self, x):
+        """Forward pass through the block."""
         out = self.conv(x)
         out = self.bn(out)
         return out
 
 
 class Block(nn.Module):
+    """Block for the CNN model, consisting of two basic blocks and a residual connection."""
     def __init__(self, inplanes, outplanes):
-        super(Block, self).__init__()
-        self.mapping = nn.Conv1d(in_channels=inplanes, out_channels=outplanes, kernel_size=1, stride=2)
+        """
+        Parameters
+        ----------
+        inplanes : int
+            Number of input channels.
+        outplanes : int
+            Number of output channels.
+        """
+        super().__init__()
+        self.mapping = nn.Conv1d(
+            in_channels=inplanes,
+            out_channels=outplanes,
+            kernel_size=1,
+            stride=2
+            )
         self.bblock1 = BasicBlock(inplanes=inplanes, outplanes=outplanes, stride=2)
         self.bblock2 = BasicBlock(inplanes=outplanes, outplanes=outplanes, stride=1)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
+        """Forward pass through the block."""
         identity = self.mapping(x)
 
         out = self.bblock1(x)
@@ -33,13 +68,28 @@ class Block(nn.Module):
 
 
 class CNNSurfaceClassifier(nn.Module):
+    """CNN-based surface classification model."""
     def __init__(self, input_size=None, output_size=None):
+        """
+        Parameters
+        ----------
+        input_size : int, optional
+            Number of input channels.
+        output_size : int, optional
+            Number of output classes.
+        """
         super().__init__()
         self.input_size = input_size
         self.output_size = output_size
 
         self.conv_pre = nn.Sequential(
-            nn.Conv1d(in_channels=self.input_size, out_channels=32, kernel_size=7, stride=2, padding=3),
+            nn.Conv1d(
+                in_channels=self.input_size,
+                out_channels=32,
+                kernel_size=7,
+                stride=2,
+                padding=3
+                ),
             nn.BatchNorm1d(num_features=32),
             nn.ReLU(inplace=True),
             nn.MaxPool1d(kernel_size=3, stride=2, padding=1),
@@ -56,6 +106,7 @@ class CNNSurfaceClassifier(nn.Module):
         )
 
     def forward(self, x):
+        """Forward pass through the model."""
         x = self.conv_pre(x)
         x = self.block1(x)
         x = self.block2(x)
